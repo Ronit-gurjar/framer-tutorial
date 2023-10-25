f43ef09a6434f6f63dd9b04197b76036fc3c7e9b
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-25 17:09:18 +0530

    Add: import and add motion div
    > [!NOTE]
    > When writing some UI code in `app/` dir, make sure use ` "use client;" ` to make sure that Next.js understand it.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+'use client';
+
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
-      <h1>hello</h1>
+      <div style={{
+        display: 'grid',
+        placeContent: "center",
+        height: "100vh",
+        gap:"0.8rem"
+      }}>
+        <motion.div>
+          hello
+        </motion.div>
+      </div>
     </>
   )
 }
