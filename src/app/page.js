2d9b33a387fca0d1185c769e981688ab1b88c548
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-28 20:05:30 +0530

    Add: animation mode using AnimatePresence

--- src/app/page.js
+++ src/app/page.js
@@ -15,22 +15,26 @@ export default function Home() {
         height: "100vh",
         gap:"0.8rem"
       }}>
-        <button className='btn' onClick={() => setIsVisible(!isVisible)}>SHOW/HIDE</button>
-        <AnimatePresence>
+        <motion.button className='btn' onClick={() => setIsVisible(!isVisible)} layout>SHOW/HIDE</motion.button>
+        <AnimatePresence mode='popLayout'>
         {isVisible && 
         (<motion.div 
           initial={{ // used to set Initiale motion of div
-            rotate:"0deg"
+            rotate:"0deg",
+            y:0
           }}
           animate={{ // used to set animated motion of div
-            rotate:"180deg"
+            rotate:"180deg",
+            y:[0,50,-150,-100, 0]
           }}
           exit={{
-            rotate:"0deg"
+            rotate:"0deg",
+            y:0
           }}
           transition={{ // used to set transition prop to motion of div
             duration: 1,
-            ease:"backInOut"          
+            ease:"backInOut",
+            times:[0,0.25,0.40,0.70,1]      
           }}
           style={{
             width:150,
