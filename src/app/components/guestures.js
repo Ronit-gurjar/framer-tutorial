bfcb2ee3dc8caddc01445e215ecaeff8bd89a1f0
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-29 09:33:52 +0530

    Add: MotionConfig for same animations

--- src/app/components/guestures.js
+++ src/app/components/guestures.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion';
+import { motion, MotionConfig } from 'framer-motion';
 
 const Guestures = () => {
   return (
@@ -9,10 +9,21 @@ const Guestures = () => {
         height: "100vh",
         gap:"0.8rem"
       }}>
+        <MotionConfig     //Instead of setting transition prop inside each button we can just use MotionConfig to set it one for all
+            transition={{
+                duration: 0.150,
+                ease: "easeInOut"
+            }}
+        >
         <motion.button className='btn'
             whileHover={{scale:1.10}} //change animation on hover
             whileTap={{scale:1.10, rotate:"3deg" ,y:10}}  //change animation on Tap
-        >button</motion.button>
+        >button 1</motion.button>
+        <motion.button className='btn'
+            whileHover={{scale:1.10}} //change animation on hover
+            whileTap={{scale:1.10, rotate:"-3deg" ,y:10}}  //change animation on Tap
+        >button 2</motion.button>
+        </MotionConfig>
     </div>
   )
 }
