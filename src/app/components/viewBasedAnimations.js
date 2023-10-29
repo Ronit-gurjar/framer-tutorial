4a702bea68079a929ee22e161e69ee15081a8b26
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-29 10:37:16 +0530

    Add: viewBasedAnimation component

---
+++ src/app/components/viewBasedAnimations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewBasedAnimations = () => {
+  return (
+    <>
+    <div style={{height: "150vh" }}>ViewBasedAnimations</div>
+    </>
+  )
+}
+
+export default ViewBasedAnimations
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@
 import AnimationControls from './components/animationControls';
 import Guestures from './components/guestures';
 import MotionBasic from './components/motionBasics'
+import ViewBasedAnimations from './components/viewBasedAnimations';
 export default function Home() {
  
   return (
     <>
       {/*<MotionBasic/>*/}
       {/*<Guestures/>*/}
-      <AnimationControls/>
+      {/*<AnimationControls/>*/}
+      <ViewBasedAnimations/>
     </>
   )
 }
