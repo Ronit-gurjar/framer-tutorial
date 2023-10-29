6c78d6a164d7bbc48565768e2a13e713a9861bc5
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-29 09:49:30 +0530

    Add: basic components

--- src/app/components/animationControls.js
+++ src/app/components/animationControls.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const AnimationControls = () => {
   return (
-    <div>AnimationControls</div>
+    <div style={{
+        display: 'grid',
+        placeContent: "center",
+        height: "100vh",
+        gap:"0.8rem"
+      }}>
+        <button className='btn'>flip it</button>
+        <div
+            style={{
+                width:150,
+                height:150,
+                background: "black"
+            }}
+        ></div>
+    </div>
   )
 }
 
